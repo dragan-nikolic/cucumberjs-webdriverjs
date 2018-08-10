0f0fc017cf870885f184da52ff1e0eb3006f99a4
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-08-10 15:04:52 +0200

    define world parameters

--- features/support/world.js
+++ features/support/world.js
@@ -1,9 +1,19 @@
+/**
+World is an isolated context for each scenario, exposed to the hooks and steps
+as 'this'.
+
+For more info refer to:
+https://github.com/cucumber/cucumber-js/blob/master/docs/support_files/world.md
+*/ 
+ 
 var {setWorldConstructor} = require('cucumber')
 var seleniumWebdriver = require('selenium-webdriver')
 
-function CustomWorld() {
+function CustomWorld({attach, parameters}) {
+  this.attach = attach
+  this.parameters = parameters
   this.driver = new seleniumWebdriver.Builder()
-                  .forBrowser('firefox')
+                  .forBrowser(parameters.browser)
                   .build();
 }
 
---
+++ run.sh
@@ -0,0 +1,3 @@
+./node_modules/.bin/cucumber-js \
+    --world-parameters "{\"browser\": \"chrome\"}" \
+    ${@:1}
